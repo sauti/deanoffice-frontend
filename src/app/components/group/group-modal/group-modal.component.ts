0c549927756dc4e10002de8b274de73a143103c2
unknown <os1998os@ukr.net>
2018-05-19 23:34:11 +0300

    added validation and finished grid

--- src/app/components/group/group-modal/group-modal.component.ts
+++ src/app/components/group/group-modal/group-modal.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Inject, Input, OnInit} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {StudentGroup} from "../../../models/StudentGroup";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {BaseReactiveFormComponent} from '../../shared/base-reactive-form/base-reactive-form.component';
 
 @Component({
   selector: 'group-modal',
@@ -9,10 +11,28 @@ import {StudentGroup} from "../../../models/StudentGroup";
 })
 export class GroupModalComponent implements OnInit {
   @Input() group: StudentGroup;
+  form: FormGroup;
 
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder) {
+
+  }
 
   ngOnInit() {
+    this.buildForm();
+
+  }
+
+   private buildForm() {
+     this.form = this.fb.group({
+       name: [this.group.name, Validators.required],
+       specialization: [this.group.specialization.name, Validators.required],
+       beginYears: [this.group.beginYears, Validators.required],
+       creationYear: [this.group.creationYear, Validators.required],
+       studySemesters: [this.group.studySemesters, Validators.required],
+       studyYears: [this.group.studyYears, Validators.required],
+       tuitionTerm: [this.group.tuitionTerm, Validators.required],
+       tuitionForm: [this.group.tuitionForm, Validators.required],
+     });
   }
 
 }

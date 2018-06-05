9b4ce2467302e7c677c625af22fcd7ed46810091
unknown <os1998os@ukr.net>
2018-06-05 13:43:25 +0300

    added select to tuition form and term

--- src/app/components/group/group-modal/group-modal.component.ts
+++ src/app/components/group/group-modal/group-modal.component.ts
@@ -1,9 +1,11 @@
 import {Component, Inject, Input, OnInit} from '@angular/core';
-import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {StudentGroup} from "../../../models/StudentGroup";
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {StudentGroup} from '../../../models/StudentGroup';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {BaseReactiveFormComponent} from '../../shared/base-reactive-form/base-reactive-form.component';
 import {GroupService} from '../../../services/group.service';
+import {TuitionForm} from '../../../models/tuition-form.enum';
+import {TuitionTerm} from '../../../models/tuition-term.enum';
 
 @Component({
   selector: 'group-modal',
@@ -24,23 +26,23 @@ export class GroupModalComponent implements OnInit {
 
   }
 
-   private buildForm() {
-     this.form = this.fb.group({
-       name: [this.group.name, Validators.required],
-       specialization: [this.group.specialization.name, Validators.required],
-       beginYears: [this.group.beginYears, Validators.required],
-       creationYear: [this.group.creationYear, Validators.required],
-       studySemesters: [this.group.studySemesters, Validators.required],
-       studyYears: [this.group.studyYears, Validators.required],
-       tuitionTerm: [this.group.tuitionTerm, Validators.required],
-       tuitionForm: [this.group.tuitionForm, Validators.required],
-     });
+  private buildForm() {
+    this.form = this.fb.group({
+      name: [this.group.name, Validators.required],
+      specialization: [this.group.specialization.name, Validators.required],
+      beginYears: [this.group.beginYears, Validators.required],
+      creationYear: [this.group.creationYear, Validators.required],
+      studySemesters: [this.group.studySemesters, Validators.required],
+      studyYears: [this.group.studyYears, Validators.required],
+      tuitionTerm: [this.group.tuitionTerm, Validators.required],
+      tuitionForm: [this.group.tuitionForm, Validators.required],
+    });
   }
 
-  updateGroup(){
+  updateGroup() {
     this.groupService.updateGroup(this.group).subscribe(response => {
-      console.log(response)
-    },
+        console.log(response)
+      },
     )
 
   }

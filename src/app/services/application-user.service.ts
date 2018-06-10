944960ac36b40b6a6632428bfef0d20bd8e8ef01
Serhii Shemshur <serhii.shemshur.dev@gmail.com>
2018-06-10 23:06:56 +0300

    Add delete, update, getById method to ApplicationUserService

--- src/app/services/application-user.service.ts
+++ src/app/services/application-user.service.ts
@@ -8,20 +8,35 @@ import {ApplicationUser} from '../models/ApplicationUser';
 
 const API_URL: string = environment.apiUrl;
 
-export const SPECIALIZATION_URL: string = API_URL + '/applicationUser';
+export const APPLICATION_USER_URL: string = API_URL + '/applicationUser';
 
 @Injectable()
 export class ApplicationUserService {
   constructor(private _httpClient: HttpClient) {}
 
   public getApplicationUsers(): Observable<ApplicationUser[]> {
-    return this._httpClient.get<ApplicationUser[]>(SPECIALIZATION_URL)
+    return this._httpClient.get<ApplicationUser[]>(APPLICATION_USER_URL)
       .pipe(catchError(forObservable('Отримання користувачів', [])));
   }
 
   create(body: ApplicationUser): Promise<any> {
-    return this._httpClient.post(SPECIALIZATION_URL, body).toPromise()
+    return this._httpClient.post(APPLICATION_USER_URL, body).toPromise()
       .catch(forPromise('Створення нового користувача'));
   }
 
+  getById(sourceId: number): Observable<ApplicationUser> {
+    return this._httpClient.get<ApplicationUser>(`${APPLICATION_USER_URL}/${sourceId}`)
+      .pipe(catchError(forObservable('Отриманная користувача по Id', [])))
+      .map(data => data as ApplicationUser)
+  }
+
+  delete(itemId: number): Promise<any> {
+    return this._httpClient.delete(`${APPLICATION_USER_URL}/${itemId}`).toPromise()
+      .catch(forPromise('Видалення користувача'));
+  }
+
+  update(body: ApplicationUser): Promise<any> {
+    return this._httpClient.put(APPLICATION_USER_URL, body).toPromise()
+      .catch(forPromise('Оновлення користувача'));
+  }
 }
